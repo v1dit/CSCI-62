e7c3266f725eff690b3052c930f7fd2098d2e9b2
VIdit <vidit@Vin-Office-MacBook-Pro.local>
2026-01-06 17:09:56 -0800

    Add hw 0 files

---
+++ hw 0/test_user_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "user.h"
+
+using namespace std;
+
+int main () {
+  User* u = new User ();
+}
