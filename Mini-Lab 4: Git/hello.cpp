494320e3e75d5e55958bb5e1e3f435f59d1fe570
vidit agarwal <vidit3035@gmail.com>
2026-02-23 16:22:39 -0800

    Add files via upload

---
+++ Mini-Lab 4: Git/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "myclass.h"
+
+using namespace std;
+
+int main () {
+  cout << "Hello World. hello world. hello world. " << endl;
+
+  MyClass a(5);
+  cout << a.getX() << endl;
+}
