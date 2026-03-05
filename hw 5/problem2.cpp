23a1027abbcea44f70fe1c424d9ddcd021e45f57
vidit agarwal <vidit3035@gmail.com>
2026-03-05 15:39:22 -0800

    Add files via upload

---
+++ hw 5/problem2.cpp
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int auth() {
+    int auth_flag=0;
+    char password[16];
+    
+    gets(password);
+    
+    if (strcmp(password, "brilling")==0)
+        auth_flag = 1;
+    
+    assert (auth_flag == 0 || auth_flag == 1);
+    return auth_flag;
+}
+
+int main(){
+    if (auth()) {
+        printf(" Access Granted.\n");
+    } else {
+        printf("\nAccess Denied.\n");
+    }
+}
