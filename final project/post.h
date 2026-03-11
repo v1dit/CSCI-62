d1684543bcc5e70906bb43b71d466b68f8c9dd76
vidit agarwal <vidit3035@gmail.com>
2026-03-10 18:13:25 -0700

    Add files via upload

---
+++ final project/post.h
@@ -0,0 +1,49 @@
+#ifndef POST_H
+#define POST_H
+
+#include <string>
+#include <set>
+
+class Post {
+public:
+    Post();
+    Post(int profileId, int authorId, std::string message, int likes);
+
+    int getMessageId() const;
+    void setMessageId(int id);
+
+    int getProfileId() const;
+    int getAuthorId() const;
+
+    std::string getMessage() const;
+    int getLikes() const;
+    bool addLike(int userId);
+    bool removeLike(int userId);
+
+    virtual std::string getURL() const;
+    virtual std::string toString() const;
+
+    virtual ~Post() {}
+
+private:
+    int messageId_;
+    int profileId_;
+    int authorId_;
+    std::string message_;
+    int likes_;
+    std::set<int> likedUsers;
+};
+
+class LinkPost : public Post {
+public:
+    LinkPost();
+    LinkPost(int profileId, int authorId, std::string message, int likes, std::string url);
+
+    std::string getURL() const override;
+    std::string toString() const override;
+
+private:
+    std::string url_;
+};
+
+#endif
